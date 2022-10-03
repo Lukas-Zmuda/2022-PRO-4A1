6210e09cf7f9b919348daf26f226f0d15bb5c59b
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-03 13:08:16 +0200

    komentare

--- MojPrvy/src/MojaPrva.java
+++ MojPrvy/src/MojaPrva.java
@@ -3,7 +3,31 @@ public class MojaPrva {
 
 	public static void main(String[] args) {
 		
+		//jednoriadkovy komentar
+		
+		/*
+		 * toto je
+		 * viacriadkovy
+		 * komentar
+		 */
+		
+		/**
+		 * javadoc
+		 * komentar
+		 */
 		System.out.println("Ahoj 4A1");
+		System.out.println("Lukas");
+		
+		System.out.printf("Ahoj %s. Dnes je %d. oktobra.\n", "Fero", 3);
+		System.out.println("-----------------");
+		
+		System.out.print("Janko ");
+		System.out.println("Hrasko");
+		
+		
+		
+		
+		
 		
 	}
 

a110ee6bb3115ca87c005e0c938b73d32fb9dc4e
Lukáš Zmuda <55268763+Lukas-Zmuda@users.noreply.github.com>
2022-10-04 10:22:44 +0200

    datove typy

---
+++ MojPrvy/src/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("  /////");
+    	System.out.println(" | o o |");
+    	System.out.println("(|  ^  |)");
+    	System.out.println(" | [_] | ");
+    	System.out.println("  ----- ");
+    	
+    	System.out.println(" /\\_/\\    ------          ");
+     	System.out.println("( ' ' )   / Hello \\");
+    	System.out.println("(  -  )  <  Junior |");
+    	System.out.println(" | | |    \\ Coder / ");
+    	System.out.println(" (_|_)      -----   ");
+
+	}
+
+}
